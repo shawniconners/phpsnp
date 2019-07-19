a04952564878e931c7f5c60c4de7486d61612c81
Shawn Conners <shawniconners@icloud.com>
2019-07-19 11:42:10 -0500

    Update collection.php
    Text and links changed.

--- collection.php
+++ collection.php
@@ -38,7 +38,7 @@
     	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     	<meta name="description" content="">
     	<meta name="author" content="">
-    	<title>phpSNP - Assemblies</title>
+    	<title>phpSNP - Collection</title>
     	<!-- Bootstrap core CSS -->
     	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.16/b-1.4.2/b-html5-1.4.2/fc-3.2.3/fh-3.1.3/sc-1.4.3/sl-1.2.3/datatables.min.css"/>
@@ -56,15 +56,15 @@
 	    <main role="main">
 	      	<div class="container">
 		        <div class="row">
-					<h2>Assemblies</h2>
-					<p class="lead">Below is a list of every assembly that has been uploaded, parsed and saved to the database. Click on an assembly to view corresponding structures and sequences.</p>
+					<h2>Collection</h2>
+					<p class="lead">Below is a list of every assembly that has been uploaded, parsed and saved to your collection. Click on an assembly to view corresponding studies, structures and sequences.</p>
 					<p class="lead"><a class="btn btn-success" href="assembly_upload.php" role="button">Upload Assembly</a></p>
 				</div>
 				<div class="row">
 					<table id="tblAssemblies" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">
 						<thead>
 							<tr>
-								<th>Name</th>
+								<th>Assembly</th>
 								<th>Studies</th>
 								<th>Structures</th>
 								<th>Bases</th>
@@ -76,7 +76,7 @@
 								//	^--- PHP -- 8A - START of looping through assemblies
 								//****************************************************************************************************************
 								foreach ($objCollection->assemblies as $arrAssembly) {
-									echo "<tr><td><a href='assembly_view.php?id=".$arrAssembly["id"]."'>".$arrAssembly["name"]."</a></td><td>".$arrAssembly["study_count"]."</td><td>".number_format($arrAssembly["structure_count"])."</td><td>".number_format($arrAssembly["sequence_length"])."</td></tr>";
+									echo "<tr><td><a href='assembly.php?assembly_id=".$arrAssembly["id"]."'>".$arrAssembly["name"]."</a></td><td>".$arrAssembly["study_count"]."</td><td>".number_format($arrAssembly["structure_count"])."</td><td>".number_format($arrAssembly["sequence_length"])."</td></tr>";
 								}
 								//****************************************************************************************************************
 								//	v--- PHP -- 8A - END of looping through assemblies
