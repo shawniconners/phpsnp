965ec6e2b7e301fd8fb39f19668390400f5b7932
Shawn Conners <shawniconners@icloud.com>
2019-07-30 16:34:34 -0500

    Navigation change

--- header.php
+++ header.php
@@ -1,7 +1,8 @@
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
 	<h5 class="my-0 mr-md-auto font-weight-normal">phpSNP</h5>
 	<nav class="my-2 my-md-0 mr-md-3">
-		<a id="elmNavigationAssemblies" class="p-2 text-dark" href="curate.php">Curate</a>
-		<a id="elmNavigationStudies" class="p-2 text-dark" href="browse.php">Browse</a>
+		<a id="elmNavigationCurate" class="p-2 text-dark" href="curate.php">Curate</a>
+		<a id="elmNavigationBrowse" class="p-2 text-dark" href="browse.php">Browse</a>
+		<a id="elmNavigationDownload" class="p-2 text-dark" href="donwload.php">Download</a>
 	</nav>
 </div>
