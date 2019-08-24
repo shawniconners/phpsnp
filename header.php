df8192f6b26589e25b7ec220255be177b73d9c2f
Shawn Conners <shawniconners@icloud.com>
2019-08-23 21:25:48 -0500

    Link fixed

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 	<nav class="my-2 my-md-0 mr-md-3">
 		<a id="elmNavigationCurate" class="p-2 text-dark" href="curate.php">Curate</a>
 		<a id="elmNavigationBrowse" class="p-2 text-dark" href="browse.php">Browse</a>
-		<a id="elmNavigationDownload" class="p-2 text-dark" href="donwload.php">Download</a>
+		<a id="elmNavigationDownload" class="p-2 text-dark" href="download.php">Download</a>
 		<a id="elmNavigationStatus" class="p-2 text-dark" href="status.php">Status</a>
 	</nav>
 </div>
